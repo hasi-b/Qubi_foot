6637b9db00b6b8893f6c04f44530da3ac985bc7a
Hasi-b <hasib.hasan9@gmail.com>
2021-07-31 15:35:18 +0600

    Array Spawner

--- Assets/Script/ObstacleSpawner.cs
+++ Assets/Script/ObstacleSpawner.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class ObstacleSpawner : MonoBehaviour
 {
     private static int obstacleCount = 2;
-    [SerializeField]
+    private static int spawnCounts = 3;
+    
    // private GameObject[] obstacles = new GameObject[obstacleCount] ;
-    private GameObject[] spawnPoints;
+    private GameObject[] spawnPoints = new GameObject[spawnCounts];
     GameObject parentSpawner;
     GameObject cone;
     GameObject post;
@@ -18,21 +19,23 @@ public class ObstacleSpawner : MonoBehaviour
         cone = (GameObject)Instantiate(Resources.Load("LP_Cone"));
         post = (GameObject)Instantiate(Resources.Load("Post"));
 
-        spawn1 = gameObject.transform.GetChild(0).gameObject;
+       /* spawn1 = gameObject.transform.GetChild(0).gameObject;
         spawn2 = gameObject.transform.GetChild(1).gameObject;
         spawn3 = gameObject.transform.GetChild(2).gameObject;
         Instantiate(cone, spawn1.transform.position, Quaternion.identity);
         Instantiate(cone, spawn2.transform.position, Quaternion.identity);
         Instantiate(cone, spawn3.transform.position, Quaternion.identity);
         // parentSpawner = GameObject.FindGameObjectWithTag("Spawner");
+       */
 
 
-        /*  for(int i = 0; i < 3; i++)
+          for(int i = 0; i < spawnCounts; i++)
           {
+            
               spawnPoints[i] = gameObject.transform.GetChild(i).gameObject;
               Instantiate(cone,spawnPoints[i].transform.position,Quaternion.identity);
           }
-        */
+        
 
 
 
