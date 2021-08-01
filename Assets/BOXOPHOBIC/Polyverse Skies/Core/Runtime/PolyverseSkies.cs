880378285beea44a4a2e68de1cc7c9f379086db1
Hasi-b <hasib.hasan9@gmail.com>
2021-08-02 04:24:30 +0600

    Revert "Revert "Day Night Cycle""
    This reverts commit cb84727db646f3444360ff6f7cab504b89064b26.

--- Assets/BOXOPHOBIC/Polyverse Skies/Core/Runtime/PolyverseSkies.cs
+++ Assets/BOXOPHOBIC/Polyverse Skies/Core/Runtime/PolyverseSkies.cs
@@ -7,6 +7,13 @@ using Boxophobic.StyledGUI;
 [ExecuteInEditMode]
 public class PolyverseSkies : StyledMonoBehaviour
 {
+
+    bool Night = false;
+    float timeStartedLerping;
+    public float LerpTime = 5f;
+    float time;
+    public float speed = 2f;
+
     [StyledBanner(0.968f, 0.572f, 0.890f, "Polyverse Skies", "", "https://docs.google.com/document/d/1z7A_xKNa2mXhvTRJqyu-ZQsAtbV32tEZQbO1OmPS_-s/edit?usp=sharing")]
     public bool styledBanner;
 
@@ -41,7 +48,35 @@ public class PolyverseSkies : StyledMonoBehaviour
         {
             skyboxMaterial = new Material(skyboxDay);
         }
+
+        StartLerping();
+
+
     }
+
+    void StartLerping()
+    {
+        timeStartedLerping = Time.time;
+       
+
+    }
+
+    public float Lerp(float start,float end,float timeStartedLerping,float lerpTime)
+    {
+        float timeSinceStarted = Time.time - timeStartedLerping;
+        float percentageCompleted = timeSinceStarted / lerpTime;
+        if(timeOfDay == 1.0f)
+        {
+
+        }
+        //Debug.Log("Percentage" + percentageCompleted);
+        float result = Mathf.Lerp(start,end,percentageCompleted);
+        return result;
+
+    }
+
+
+
 
     void Update()
     {
@@ -73,6 +108,44 @@ public class PolyverseSkies : StyledMonoBehaviour
         {
             DynamicGI.UpdateEnvironment();
         }
+
+
+        /*  if (timeOfDay == 1.0f)
+          {
+              Night = true;
+              StartLerping();
+              Debug.Log("NIGHT TRUE");
+          }
+          if(timeOfDay == 0.0f)
+          {
+              Night = false;
+
+              Debug.Log("NIGHT False");
+          }
+
+
+
+             timeOfDay = Lerp(0, 1, timeStartedLerping, LerpTime);
+        */
+
+        float timeSinceStarted = Time.time - timeStartedLerping;
+        float percentageCompleted = timeSinceStarted / LerpTime;
+        time = Mathf.PingPong(percentageCompleted*speed,1);
+        Debug.Log("Time"+ time);
+        timeOfDay = time;
+       
+           
+            
+
+       
+        
+
+
+
+
+
+
+
     }
 
 #if UNITY_EDITOR
